27d9ac23ce1c0d8349541be08d44263aeb61123d
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2019-11-18 12:25:56 +0300

    add commands CLient->Naming Server

---
+++ Commons/src/main/java/commons/commands/naming/InfoFile.java
@@ -0,0 +1,13 @@
+package commons.commands.naming;
+
+public class InfoFile extends NamingCommand {
+    private String remotePath;
+
+    public InfoFile(String remotePath) {
+        this.remotePath = remotePath;
+    }
+
+    public String getRemotePath() {
+        return remotePath;
+    }
+}
