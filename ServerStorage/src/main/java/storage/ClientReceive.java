c21d79f2d5bc2a12a06ce1bcba0ffdb6f2555d25
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-11-18 20:30:33 +0300

    Add replication to download process

--- ServerStorage/src/main/java/storage/ClientReceive.java
+++ ServerStorage/src/main/java/storage/ClientReceive.java
@@ -28,27 +28,42 @@ public class ClientReceive extends Thread {
 		IORoutines.sendSignalOnce(address, Ports.PORT_NAMING, uploadAck); // FIXME Replace port
 	}
 
-	private void notifyClient(int status, UUID uuid) throws IOException {
-		FileUploadAck uploadAck = new FileUploadAck(status, uuid);
+	private void notifyClientFail(UUID uuid) throws IOException {
+		FileUploadAck uploadAck = new FileUploadAck(0, uuid);
 		IORoutines.sendSignal(conn, uploadAck);
+	}
+
+	private OutputStream[] getReplicaOutputStreams(OutputStream fileOut, InetAddress[] addresses) throws IOException {
+		OutputStream[] streams = new OutputStream[addresses.length + 1];
+		FileUpload uploadCommand = new FileUpload(command.getUuid());
+		streams[0] = fileOut;
+		for (int i = 1; i <= addresses.length; i++) {
+			Socket socket = new Socket(addresses[i], Ports.PORT_STORAGE);
+			IORoutines.sendSignal(socket, uploadCommand);
+			streams[i] = socket.getOutputStream();
+		}
+		return streams;
 	}
 
 	private void receiveFile() {
 		InputStream sockIn;
 		OutputStream sockOut;
 		FileOutputStream fileOut;
+		OutputStream[] sockReplica;
 
 		try {
 			fileOut = new FileOutputStream(command.getUuid().toString());
 			sockIn = conn.getInputStream();
 			sockOut = conn.getOutputStream();
+			sockReplica = getReplicaOutputStreams(fileOut, command.getReplicaAddresses());
 		} catch (IOException ex) {
 			ex.printStackTrace();
+			System.err.println("RECEIVE: Could not get/initialize streams for receiving files.");
 			return;
 		}
 
 		try {
-			IORoutines.transmit(sockIn, fileOut);
+			IORoutines.transmitSplit(sockIn, sockReplica);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
@@ -59,16 +74,16 @@ public class ClientReceive extends Thread {
 				sockOut.close();
 			} catch (IOException ex) {
 				ex.printStackTrace();
-				System.err.println("IOException thrown while trying to close streams.");
+				System.err.println("RECEIVE: IOException thrown while trying to close streams.");
 			}
 		}
 
 		try {
 			notifyNaming(0, command.getUuid());
-			notifyClient(0, command.getUuid());
+			notifyClientFail(command.getUuid());
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.err.println("Could not notify naming server nor client about fail.");
+			System.err.println("RECEIVE: Could not notify naming server nor client about fail.");
 		}
 	}
 
