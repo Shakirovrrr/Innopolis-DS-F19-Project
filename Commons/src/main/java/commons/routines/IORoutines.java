a1c2d8889baf042d5d2648d662cb3cb046900a89
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-11-18 11:36:36 +0300

    Add splitted transmit into IORoutines

--- Commons/src/main/java/commons/routines/IORoutines.java
+++ Commons/src/main/java/commons/routines/IORoutines.java
@@ -5,11 +5,25 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class IORoutines {
+	public static void transmit(InputStream streamFrom, OutputStream streamTo) throws IOException {
+		transmit(streamFrom, streamTo, 8192);
+	}
+
 	public static void transmit(InputStream streamFrom, OutputStream streamTo, int bufferSize) throws IOException {
+		transmitSplit(streamFrom, bufferSize, streamTo);
+	}
+
+	public static void transmitSplit(InputStream streamFrom, OutputStream... streamTo) throws IOException {
+		transmitSplit(streamFrom, 8192, streamTo);
+	}
+
+	public static void transmitSplit(InputStream streamFrom, int bufferSize, OutputStream... streamTo) throws IOException {
 		int count;
 		byte[] buffer = new byte[bufferSize];
 		while ((count = streamFrom.read(buffer)) > 0) {
-			streamTo.write(buffer, 0, count);
+			for (OutputStream to : streamTo) {
+				to.write(buffer, 0, count);
+			}
 		}
 	}
 }
