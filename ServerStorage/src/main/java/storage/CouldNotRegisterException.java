c1abfa82f9d1be057d99ff9ca6dbea21947a0283
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-11-27 18:37:29 +0300

    Implement registration process

---
+++ ServerStorage/src/main/java/storage/CouldNotRegisterException.java
@@ -0,0 +1,7 @@
+package storage;
+
+class CouldNotRegisterException extends Exception {
+	CouldNotRegisterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
--- ServerStorage/src/main/java/storage/Main.java
+++ ServerStorage/src/main/java/storage/Main.java
@@ -2,15 +2,22 @@ package storage;
 
 import commons.Ports;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.UUID;
 
 class Main {
 	static UUID nodeUuid;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CouldNotRegisterException {
 		System.out.println("Hello Storage!");
 
 		nodeUuid = UUID.randomUUID();
+		try {
+			Register.register(InetAddress.getByName("192.168.0.1"));
+		} catch (IOException e) {
+			throw new CouldNotRegisterException("Could not register at naming server.", e);
+		}
 
 		ClientDispatcher dispatcher = new ClientDispatcher(Ports.PORT_STORAGE);
 		dispatcher.start();
