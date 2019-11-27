7b15d5ab190b9321247e4e2d32756e938b4e5b5d
Rim Rakhimov <rimrakhimov@gmail.com>
2019-11-27 17:39:25 +0300

    class for managing file tree

---
+++ ServerNaming/src/main/java/naming/FileManager.java
@@ -0,0 +1,11 @@
+package naming;
+
+public class FileManager {
+    FileTree fileTree;
+
+    public FileManager() {
+        this.fileTree = new FileTree("uniuser");
+    }
+
+    public addFile(Path path)
+}
