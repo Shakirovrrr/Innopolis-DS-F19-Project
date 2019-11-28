0aaca1d3855e431e079a4b4ea9307a4976757cc6
Rim Rakhimov <rimrakhimov@gmail.com>
2019-11-28 16:58:29 +0300

    lsAck added

--- Commons/src/main/java/commons/commands/naming/LsAck.java
+++ Commons/src/main/java/commons/commands/naming/LsAck.java
@@ -1,5 +1,25 @@
 package commons.commands.naming;
 
-public class LsAck extends NamingCommandAck {
+import naming.File;
+import naming.Folder;
 
+import java.util.List;
+
+public class LsAck extends NamingCommandAck {
+    private List<Folder> folders;
+    private List<File> files;
+
+    public LsAck(int statusCode, List<Folder> folders, List<File> files) {
+        this.status = statusCode;
+        this.folders = folders;
+        this.files = files;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
 }
