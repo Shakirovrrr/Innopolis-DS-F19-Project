6eaf1c0d7c6f4191f132f9c621fb114f90e46e30
Rim Rakhimov <rimrakhimov@gmail.com>
2019-11-30 20:05:29 +0300

    error acknowledgement

--- Commons/src/main/java/commons/commands/naming/ErrorAck.java
+++ Commons/src/main/java/commons/commands/naming/ErrorAck.java
@@ -2,6 +2,7 @@ package commons.commands.naming;
 
 import commons.StatusCodes;
 
+@Deprecated(forRemoval = true)
 public class ErrorAck extends NamingCommandAck {
     public ErrorAck() {
         this.status = StatusCodes.Code.UNKNOWN_COMMAND;
