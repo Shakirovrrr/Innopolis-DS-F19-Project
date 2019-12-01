f3f1d305885cf9769279776e05f8460dc274b3a9
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-12-01 18:54:51 +0300

    Delete ErrorAck.java (obsolete)

--- Commons/src/main/java/commons/commands/naming/ErrorAck.java
+++
@@ -1,9 +0,0 @@
-package commons.commands.naming;
-
-import commons.StatusCodes;
-
-public class ErrorAck extends NamingCommandAck {
-    public ErrorAck() {
-        this.status = StatusCodes.Code.UNKNOWN_COMMAND;
-    }
-}
