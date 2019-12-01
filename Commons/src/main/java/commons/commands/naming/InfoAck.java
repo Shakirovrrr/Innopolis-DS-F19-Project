f73f0f716e3d942f5118c9b31d2c3ab492ae99b6
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 13:00:13 +0300

    path removed from infoack

--- Commons/src/main/java/commons/commands/naming/InfoAck.java
+++ Commons/src/main/java/commons/commands/naming/InfoAck.java
@@ -6,19 +6,13 @@ import java.nio.file.Path;
 import java.util.UUID;
 
 public class InfoAck extends NamingCommandAck {
-    private Path path;
     private int fileSize;
     private int accessRights;
 
-    public InfoAck(int statusCode, Path path, int fileSize, int accessRights) {
+    public InfoAck(int statusCode, int fileSize, int accessRights) {
         super(statusCode);
-        this.path = path;
         this.fileSize = fileSize;
         this.accessRights = accessRights;
-    }
-
-    public Path getPath() {
-        return path;
     }
 
     public int getFileSize() {
