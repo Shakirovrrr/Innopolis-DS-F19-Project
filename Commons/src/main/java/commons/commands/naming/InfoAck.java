e2e4b1cb22bb5bfa00068ad6e9c4e169cf9797b7
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 13:03:37 +0300

    info acknowledgement mod

--- Commons/src/main/java/commons/commands/naming/InfoAck.java
+++ Commons/src/main/java/commons/commands/naming/InfoAck.java
@@ -3,23 +3,37 @@ package commons.commands.naming;
 import commons.StatusCodes;
 
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.UUID;
 
 public class InfoAck extends NamingCommandAck {
-    private int fileSize;
+    private long fileSize;
     private int accessRights;
+    private UUID[] nodes;
 
-    public InfoAck(int statusCode, int fileSize, int accessRights) {
+    public InfoAck(int statusCode, long fileSize, int accessRights, UUID[] nodes) {
         super(statusCode);
         this.fileSize = fileSize;
         this.accessRights = accessRights;
+        this.nodes = nodes;
     }
 
-    public int getFileSize() {
+    public InfoAck(int statusCode, long fileSize, int accessRights, Collection<UUID> nodes) {
+        super(statusCode);
+        this.fileSize = fileSize;
+        this.accessRights = accessRights;
+        this.nodes = nodes.toArray(new UUID[0]);
+    }
+
+    public long getFileSize() {
         return fileSize;
     }
 
     public int getAccessRights() {
         return accessRights;
+    }
+
+    public UUID[] getNodes() {
+        return nodes;
     }
 }
