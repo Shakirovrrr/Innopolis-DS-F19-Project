92ab8d717baa1adfbc461bfb16711f35d95fb137
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 18:50:04 +0300

    rmAck added

---
+++ Commons/src/main/java/commons/commands/naming/RmAck.java
@@ -0,0 +1,9 @@
+package commons.commands.naming;
+
+import commons.StatusCodes;
+
+public class RmAck extends NamingCommandAck {
+    public RmAck(int statusCode) {
+        super(statusCode);
+    }
+}
