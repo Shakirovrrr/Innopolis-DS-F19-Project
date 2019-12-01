d009fab1bf65e998f56a179b6ac05b4468a33446
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 19:25:13 +0300

    RegisterNodeAck added

---
+++ Commons/src/main/java/commons/commands/internal/RegisterNodeAck.java
@@ -0,0 +1,13 @@
+package commons.commands.internal;
+
+public class RegisterNodeAck extends InternalCommand {
+    private int status;
+
+    public RegisterNodeAck(int statusCode) {
+        this.status = statusCode;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
