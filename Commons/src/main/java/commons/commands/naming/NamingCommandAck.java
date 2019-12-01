2c298d3bbb706ce79103fe2d7b4de8abb16e9d5b
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 13:48:46 +0300

    deprecated status code removed

--- Commons/src/main/java/commons/commands/naming/NamingCommandAck.java
+++ Commons/src/main/java/commons/commands/naming/NamingCommandAck.java
@@ -3,22 +3,13 @@ package commons.commands.naming;
 import commons.StatusCodes;
 
 public abstract class NamingCommandAck extends NamingCommand {
-	@Deprecated
-	StatusCodes.Code status;
-	int statusCode;
-	StatusCodes statusCodes = new StatusCodes();
+	private int statusCode;
 
-	@Deprecated
-	public StatusCodes.Code getStatus() {
-		return status;
+	public NamingCommandAck(int statusCode) {
+		this.statusCode = statusCode;
 	}
 
 	public int getStatusCode() {
 		return statusCode;
-	}
-
-	public String getStatusStr(){
-		return this.statusCodes.getStatusCode(statusCode);
-
 	}
 }
