1ece08411a9b5d54a2fb171c0bb84959ee276c76
Rim Rakhimov <rimrakhimov@gmail.com>
2019-12-01 12:28:37 +0300

    addition of nodeId undone

--- Commons/src/main/java/commons/commands/internal/InternalCommand.java
+++ Commons/src/main/java/commons/commands/internal/InternalCommand.java
@@ -2,16 +2,6 @@ package commons.commands.internal;
 
 import commons.commands.Command;
 
-import java.util.UUID;
-
 public abstract class InternalCommand extends Command {
-    private UUID nodeId;
 
-    public InternalCommand(UUID nodeId) {
-        this.nodeId = nodeId;
-    }
-
-    public UUID getNodeId() {
-        return nodeId;
-    }
 }
